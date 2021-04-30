72c099f7e1c21e9cf0e9015b51dff0549630856e
ColsonB <bcolson@la-providence.net>
2021-04-30 11:34:23 +0200

    Commit

--- jeu.h
+++ jeu.h
@@ -128,13 +128,67 @@ void jouer(sf::RenderWindow* ecran)
 	// Effacement de l'écran
 	window.clear(ecran, NULL, /*SDL_MapRGB(ecran->format, 255, 255, 255)*/);
 
+	// Placement des objets à l'écran
+	objectifsRestants = 0;
+
+	for (i = 0; i < NB_BLOCS_LARGEUR; i++)
+	{
+		for (j = 0; j < NB_BLOCS_HAUTEUR; j++)
+		{
+			position.x = i * TAILLE_BLOC;
+			position.y = j * TAILLE_BLOC;
+
+			switch (carte[i][j])
+			{
+			case MUR:
+				sf::RenderWindow(mur, NULL, ecran, &position);
+				break;
+			case CAISSE:
+				sf::RenderWindow(caisse, NULL, ecran, &position);
+				break;
+			case CAISSE_OK:
+				sf::RenderWindow(caisseOK, NULL, ecran, &position);
+				break;
+			case OBJECTIF:
+				sf::RenderWindow(objectif, NULL, ecran, &position);
+				objectifsRestants = 1;
+				break;
+			}
+		}
+	}
+
+	// Si on n'a trouvé aucun objectif sur la carte, c'est qu'on a gagné
+	if (!objectifsRestants)
+		continuer = 0;
+
+	// On place le joueur à la bonne position
+	position.x = positionJoueur.x * TAILLE_BLOC;
+	position.y = positionJoueur.y * TAILLE_BLOC;
+	sf::RenderWindow(marioActuel, NULL, ecran, &position);
 }
 
 void deplacerJoueur(int carte[][NB_BLOCS_HAUTEUR], sf::Transformable *pos, int direction)
 {
+	switch (direction)
+	{
+		if (pos->y - 1 < 0) // Si le joueur dépasse l'écran, on arrête
+			break;
 
+		if (carte[pos->x][pos->y - 1] == MUR) // S'il y a un mur, on arrête
+			break;
 
+		// Si on veut pousser une caisse, il faut vérifier qu'il n'y a pas de mur derrière (ou une autre caisse, ou la limite du monde)
+		if ((carte[pos->x][pos->y - 1] == CAISSE || carte[pos->x][pos->y - 1] == CAISSE_OK) &&
+			(pos->y - 2 < 0 || carte[pos->x][pos->y - 2] == MUR ||
+				carte[pos->x][pos->y - 2] == CAISSE || carte[pos->x][pos->y - 2] == CAISSE_OK))
+			break;
+	}
 
+	// Si on arrive là, c'est qu'on peut déplacer le joueur !
+	// On vérifie d'abord s'il y a une caisse à déplacer
+	deplacerCaisse(&carte[pos->x][pos->y - 1], &carte[pos->x][pos->y - 2]);
+
+	pos->y--;
 
 }
 
